f7a44aa1c50657c140b210d1be78c4f409c62d9a
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-05-20 13:01:47 +0700

    Add @Data and @Builder annotations to UserDTO

--- src/main/java/com/swp391/database_prototype/dto/UserDTO.java
+++ src/main/java/com/swp391/database_prototype/dto/UserDTO.java
@@ -2,10 +2,14 @@ package com.swp391.database_prototype.dto;
 
 import com.swp391.database_prototype.pojos.Gender;
 import com.swp391.database_prototype.pojos.Role;
+import lombok.Builder;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Data
+@Builder
 public class UserDTO {
     private String id;
     private String email;
