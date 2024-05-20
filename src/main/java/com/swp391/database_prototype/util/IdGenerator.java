768e83294065c8cced4a8edca1f5a2041783c958
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-05-20 12:42:17 +0700

    Push Initial setup of spring boot and Hibernate Add pojos Account, Role, UserInfo

--- src/main/java/com/swp391/database_prototype/util/IdGenerator.java
+++ src/main/java/com/swp391/database_prototype/util/IdGenerator.java
@@ -1,24 +1,15 @@
 package com.swp391.database_prototype.util;
 
-import com.sun.jdi.LongType;
-import jakarta.persistence.Query;
-import org.hibernate.HibernateException;
 import org.hibernate.MappingException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.generator.EventType;
-import org.hibernate.id.Configurable;
-import org.hibernate.id.IdentifierGenerator;
 import org.hibernate.id.enhanced.SequenceStyleGenerator;
 import org.hibernate.internal.util.config.ConfigurationHelper;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.type.Type;
 import org.hibernate.type.spi.TypeConfiguration;
 
-import javax.lang.model.type.TypeKind;
-import java.util.List;
 import java.util.Properties;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
 public class IdGenerator extends SequenceStyleGenerator {
     public static final String VALUE_PREFIX_PARAMETER = "valuePrefix";
